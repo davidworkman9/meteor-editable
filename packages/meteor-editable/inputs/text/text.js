271d319d8f1f6cce21898110d5fafc48dc54fba7
Dave Workman <davidworkman9@gmail.com>
2014-04-25 16:44:23 -0400

    display range differently, not sure where to display the value yet

--- packages/meteor-editable/inputs/text/text.js
+++ packages/meteor-editable/inputs/text/text.js
@@ -19,3 +19,42 @@ _.each(types, function (t) {
         }
     });
 });
+
+Template.m_editable_form_text.events({
+    'input input[type="range"]': function (e, tmpl) {
+        tmpl.$('.output').text(tmpl.$(e.target).val());
+    }
+});
+Template.m_editable_form_text.helpers({
+    'hasOutput':        function () { return this.type === 'range'; },
+    'formControlClass': function () { return this.type !== 'range' ? 'form-control' : 'input-medium'; }
+});
+
+/*
+ Template.m_editable_form_text.destroyed = function () { this.Session.destroyAll(); this.Deps.stopAll(); };
+ Template.m_editable_form_text.created = function () {
+    var self = this;
+
+    self.Deps = {
+        _handles: [],
+        stopAll: function () { _.each(this._handles, function (d) { d.stop(); }); },
+        autorun: function (f) { this._handles.push(Deps.autorun(f)); }
+    };
+
+    self._sessId = Random.id();
+    self.Session = {
+        destroyAll: function () {
+            _.each(Object.keys(Session.keys), function (key) {
+                var sessCheck = new RegExp('-' + self._sessId + '$');
+                if(sessCheck.test(key)) {
+                    Session.set([key]);
+                    delete Session.keys[key];
+                }
+            });
+        },
+        set: function (key, val) { return Session.set(key + '-' + self._sessId, val); },
+        get: function (key) { return Session.get(key + '-' + self._sessId); },
+        equals: function (key, val) { return Session.equals(key + '-' + self._sessId, val); }
+    };
+};
+*/
