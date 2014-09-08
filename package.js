5712ae23daed42cd1ce9046bd3167f2eaaedfa73
Dave Workman <davidworkman9@gmail.com>
2014-09-08 15:41:17 -0400

    bumped version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor-Editable - in-place edit package influenced by x-editable",
-    version: "0.1.2",
+    version: "0.1.3",
     git: "https://github.com/davidworkman9/meteor-editable.git",
     name: "workman:meteor-editable"
 });
